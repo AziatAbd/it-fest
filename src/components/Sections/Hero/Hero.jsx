0b24acd4477b18030ca637ebf473b7d8ab3246c3
Aziat <115453436+AziatAbd@users.noreply.github.com>
2024-10-03 14:38:11 +0600

    Update Hero.jsx

--- src/components/Sections/Hero/Hero.jsx
+++ src/components/Sections/Hero/Hero.jsx
@@ -2,12 +2,7 @@ import "./Hero.css";
 export const Hero = () => {
   return (
     <>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
-        cumque voluptas aperiam? Doloremque exercitationem ratione perspiciatis
-        numquam nisi, qui doloribus consequuntur eligendi error sapiente a
-        dolores, accusamus quisquam non aliquam!
-      </p>
+    
     </>
   );
 };
