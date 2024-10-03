9667155691ff9c47d7e0edad8a41da27492912fb
AziatAbd <abdimalikovaziat@gmail.com>
2024-10-03 14:15:46 +0600

    create input

--- src/components/Sections/Hero/Hero.jsx
+++ src/components/Sections/Hero/Hero.jsx
@@ -1,4 +1,20 @@
 import "./Hero.css";
 export const Hero = () => {
-  return <></>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae id
+        quo repellat expedita laudantium deserunt harum facilis vitae, quis eos
+        distinctio fugit, aliquid magnam, alias laboriosam blanditiis quaerat
+        quia adipisci.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere unde,
+        similique architecto a laudantium nisi deserunt neque distinctio
+        asperiores dolores vitae hic ad quos quasi reprehenderit consequatur,
+        iusto vero ea.
+      </p>
+    </>
+  );
 };
